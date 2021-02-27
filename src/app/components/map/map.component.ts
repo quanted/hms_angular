24a43539557921775f515640e8eb25cd089138af
Gavin <jasondunken@yahoo.com>
2021-02-27 12:46:02 -0500

    refactored basemap handling

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -12,10 +12,11 @@ import * as ESRI from 'esri-leaflet';
 export class MapComponent {
   @Output() mapClick: EventEmitter<any> = new EventEmitter<any>();
 
-  layers_URLS = [
+  defaultFeatureLayer = 'huc8';
+  features_URLS = [
     { 
       name: 'flowlines',
-      color: 'blue',
+      color: '#00FFFF',
       weight: 1,
       fillOpacity: 0,
       url: "https://watersgeo.epa.gov/arcgis/rest/services/NHDPlus_NP21/NHDSnapshot_NP21/MapServer/0",
@@ -29,68 +30,76 @@ export class MapComponent {
     },
     { 
       name: 'huc12',
-      color: 'red',
+      color: '#0026FF',
       weight: 1,
       fillOpacity: 0,
       url: "https://watersgeo.epa.gov/arcgis/rest/services/NHDPlus_NP21/WBD_NP21_Simplified/MapServer/0"
     },
     { 
       name: 'huc10', 
-      color: 'red',
+      color: '#4800FF',
       weight: 1,
       fillOpacity: 0,
       url: "https://watersgeo.epa.gov/arcgis/rest/services/NHDPlus_NP21/WBD_NP21_Simplified/MapServer/1"
     },
     { 
       name: 'huc8', 
-      color: 'red',
-      weight: 1,
+      color: '#B200FF',
+      weight: 2,
       fillOpacity: 0,
       url: "https://watersgeo.epa.gov/arcgis/rest/services/NHDPlus_NP21/WBD_NP21_Simplified/MapServer/2"
     },
     { 
       name: 'huc6', 
-      color: 'red',
-      weight: 1,
+      color: '#FF00DC',
+      weight: 4,
       fillOpacity: 0,
       url: "https://watersgeo.epa.gov/arcgis/rest/services/NHDPlus_NP21/WBD_NP21_Simplified/MapServer/3"
     },
     { 
       name: 'huc4', 
-      color: 'red',
-      weight: 1,
+      color: '#FF006E',
+      weight: 6,
       fillOpacity: 0,
       url: "https://watersgeo.epa.gov/arcgis/rest/services/NHDPlus_NP21/WBD_NP21_Simplified/MapServer/4"
     },
     { 
       name: 'huc2', 
-      color: 'red',
-      weight: 1,
+      color: '#FF0000',
+      weight: 8,
       fillOpacity: 0,
       url: "https://watersgeo.epa.gov/arcgis/rest/services/NHDPlus_NP21/WBD_NP21_Simplified/MapServer/5"
     },
   ]
 
+  defaultBasemap = 'Open Street Map';
+  basemaps = [
+    {
+      name: 'Open Street Map',
+      layer: L.tileLayer(
+        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+        {
+          attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        })
+    },
+    {
+      name: 'Open Topo Map',
+      layer: L.tileLayer(
+        'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', 
+        {
+          attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+        })
+    },
+    {
+      name: 'No basemap',
+      layer: L.tileLayer('',
+        {
+          attribution: '(<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+        })
+    }
+  ]
+
   map: L.Map;
-
-  openStreetMap = L.tileLayer(
-    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-    {
-      attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-    }
-  );
-
-  openTopoMap = L.tileLayer(
-    'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', 
-    {
-      attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-    }
-  );
-
-  emptyBasemap = L.tileLayer('',
-  {
-    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-  });
 
   // flag = L.icon({
   //   iconUrl: "../assets/images/icon_flag.png",
@@ -98,22 +107,37 @@ export class MapComponent {
   //   iconAnchor: [16, 16], // point of the icon which will correspond to marker's location
   // });
 
-  baseLayers = {
-    "Open Street Map": this.openStreetMap,
-    "Open Topo Map": this.openTopoMap,
-    "No basemap": this.emptyBasemap,
-  };
-
-  overlays = {
+  featureLayers = {
   //   "Marker": marker,
   //   "Roads": roadsLayer
   };
+
+  basemapLayers = {
+    // "Map Name": "map layer",
+  }
 
   constructor(
   ) {}
 
   ngOnInit() {
-    for (let url of this.layers_URLS) {
+    if(!this.map) {
+      this.map = L.map("map", {
+        center: [37.31, -92.1],  // US geographical center
+        zoom: 8,
+        minZoom: 5,
+      });
+      this.map.on("click", ($event) => {
+        this.handleClick($event);
+      });
+      this.map.on('zoomend', ($event) => {
+        this.handleZoom($event);
+      });
+    }
+    for (let basemap of this.basemaps) {
+      this.basemapLayers[basemap.name] = basemap.layer;
+    }
+    this.basemapLayers[this.defaultBasemap].addTo(this.map);
+    for (let url of this.features_URLS) {
           let layer = ESRI.featureLayer({
             url: url.url,
           });
@@ -122,24 +146,11 @@ export class MapComponent {
             weight: url.weight,
             fillOpacity: url.fillOpacity,
           });
-          this.overlays[url.name] = layer;
+          this.featureLayers[url.name] = layer;
     }
-    if(!this.map) {
-      this.map = L.map("map", {
-        center: [37.31, -92.1],  // US geographical center
-        zoom: 8,
-      });
-      this.map.on("click", ($event) => {
-        this.handleClick($event);
-      });
-      this.map.on('zoomend', ($event) => {
-        this.handleZoom($event);
-      });
-      this.openStreetMap.addTo(this.map);
-    }
-    this.overlays['huc8'].addTo(this.map);
+    this.featureLayers[this.defaultFeatureLayer].addTo(this.map);
       
-    L.control.layers(this.baseLayers, this.overlays, {
+    L.control.layers(this.basemapLayers, this.featureLayers, {
       collapsed: false,
     }).addTo(this.map);
     L.control.scale().addTo(this.map);
@@ -159,8 +170,8 @@ export class MapComponent {
   }
 
   handleZoom($event) {
-    // console.log('zoom: ', $event);
     let zoom = this.map.getZoom();
+    console.log('zoom: ', zoom);
   }
 }
 
