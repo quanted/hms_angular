5059b3feef3b72282cd451747c53781603defc48
Jason Dunken <jasondunken@yahoo.com>
2021-03-21 10:54:28 -0400

    refactor apiEndpointList object

--- src/app/components/input/meteorology/meteorology.component.ts
+++ src/app/components/input/meteorology/meteorology.component.ts
@@ -17,7 +17,7 @@ export class MeteorologyComponent implements OnInit {
   schemas;
 
   currentEndpoint;
-  inputs = [];
+  formInputs = [];
 
   constructor(
     private hms: HmsService,
@@ -43,35 +43,38 @@ export class MeteorologyComponent implements OnInit {
 
     this.apiEndpointList = [];
     for (let apiPath of Object.keys(swagger.paths)) {
+      // TODO this needs a lot of work to properly build a list of endpoints and parameters
       let requestType = swagger.paths[apiPath].hasOwnProperty('post')? 'post' : swagger.paths[apiPath].hasOwnProperty('get')? 'get' : 'null';
       let request = swagger.paths[apiPath];
       this.apiEndpointList.push({
         endpoint: apiPath,
         urlParts: apiPath.split('/').slice(1), 
         type: requestType,
-        request: request[requestType] 
+        summary: request[requestType].summary,
+        request: request[requestType]?.requestBody?.content['application/json']?.example
       });
     }
   }
 
   updateForm(): void {
     let endpoint = this.metForm.get('endpointSelect').value;
-    this.inputs = [];
-    const formInputs = {};
+    this.formInputs = [];
+    const formBuilderInputs = {};
     if (endpoint !== 'null') {
-      for (let ept of this.apiEndpointList) {
-        if (this.metForm.get('endpointSelect').value == ept.endpoint) {
-          endpoint = ept;
-          const example = endpoint.request?.requestBody?.content['application/json']?.example;
-          for (let key of Object.keys(example)) {
-            this.inputs.push(key);
-            formInputs[key] = [null] 
+      // TODO this will also need a bunch of attention once a more complex endpoint list arrives
+      for (let apiEndpoint of this.apiEndpointList) {
+        if (this.metForm.get('endpointSelect').value == apiEndpoint.endpoint) {
+          endpoint = apiEndpoint;
+          for (let key of Object.keys(endpoint.request)) {
+            this.formInputs.push(key);
+            formBuilderInputs[key] = [null]
           }
         }
       }
     }
-    this.endpointForm = this.fb.group(formInputs);
+    this.endpointForm = this.fb.group(formBuilderInputs);
     this.currentEndpoint = endpoint !== 'null'? endpoint : null;
+    console.log('endpoint: ', endpoint);
     console.log('schemas: ', this.schemas);
   }
 
