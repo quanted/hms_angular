82531ee80b25f07715988f2145f6797a2c2da547
Jason Dunken <jasondunken@yahoo.com>
2021-04-23 11:30:41 -0400

    refactor data array creation

--- src/app/components/output/table/table.component.ts
+++ src/app/components/output/table/table.component.ts
@@ -11,15 +11,15 @@ export class TableComponent implements OnInit {
     private session: SessionService
   ) { }
 
-  items = {};
-
   columnNames = [];
   columnData = [];
 
   ngOnInit(): void {
-    this.items = this.session.getData();
-    const metadata = this.items['metadata'];
+    const items = this.session.getData();
+    const metadata = items['metadata'];
     const metaKeys = Object.keys(metadata);
+    const data = items['data'];
+    const dataKeys = Object.keys(data);
 
     /* Building the column names array */
     // we know that all columns have a string name that is stored in the metadata
@@ -48,53 +48,14 @@ export class TableComponent implements OnInit {
       col = metaKeys[key];
     }
 
-    // for (let key of metaKeys) {
-    //   for (let i = 1; i < metaKeys.length; i++){
-    //     if (key.startsWith('column_' + i)) {
-    //       if (columns['column_' + i] === undefined) {
-    //         columns['column_' + i] = metadata[key];
-    //         columnArray.push(['column_' + i, metadata[key]]);
-    //         sortedArray = columnArray.sort( function(a,b) {
-    //           if (a[0] < b[0]) return -1;
-    //           if (a[0] > b[0]) return 1;
-    //           return 0
-    //         });
-    //       } else {
-    //         columnArray[i - 1][1] = columns['column_' + i] + " (" + metadata[key] + ")";
-    //       }
-    //     }
-    //   }
-    // }
-
-    // console.log(sortedArray);
-    
-    // for (let key of Object.keys(columns)) {
-    //   this.columnNames.push(sortedArray);
-    // }
-
-    // sortedArray.forEach(value => {
-    //   this.columnNames.push(value[1])
-    // });
-
-    // console.log("Column Names:" + this.columnNames)
-
     /* building the data array */
-    const keys = Object.keys(this.items['data']);
-    const data = [];
-
-    for (let i = 0; i < keys.length; i++) {
-      data.push(this.items['data'][keys[i]]);
-    }
-    
-    for (let i = 0; i < data.length; i ++) {
+    for (let i = 0; i < dataKeys.length; i ++) {
       const record = [];
-      record.push(keys[i]);
-
-      for (let j = 0; j < data[i].length; j++) {
-        record.push(data[i][j]);
+      record.push(dataKeys[i]);
+      for (let j = 0; j < data[dataKeys[i]].length; j++) {
+        record.push(data[dataKeys[i]][j]);
       }
       this.columnData.push(record);
     }
-    console.log('columnData: ', this.columnData);
   }
 }
