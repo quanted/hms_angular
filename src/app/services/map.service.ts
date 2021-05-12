7f5e33fcf231004e3bf08470a51cd8e71be1fff2
Jason Dunken <jasondunken@yahoo.com>
2021-05-12 19:03:12 -0400

    rename functions

--- src/app/services/map.service.ts
+++ src/app/services/map.service.ts
@@ -21,6 +21,8 @@ export class MapService {
 
   defaultBasemap = "Open Street Map";
 
+  watersUrl = "https://ofmpub.epa.gov/waters10/";
+
   map: L.Map;
   hoverLayer: L.GeoJSON = null;
   hucLayer: L.GeoJSON = null;
@@ -170,11 +172,11 @@ export class MapService {
     if (this.stationLayer !== null) {
       this.map.removeLayer(this.stationLayer);
     }
-    this.getStream(
+    this.getStreamStart(
       mapClickEvent.latlng.lat,
       mapClickEvent.latlng.lng
     ).subscribe((response) => {
-      this.get_callback(null, response);
+      this.getStreamSegments(null, response);
     });
   }
 
@@ -188,11 +190,9 @@ export class MapService {
 
   handleZoom(mapZoomEvent) {
     let zoom = this.map.getZoom();
-    // console.log("zoom: ", zoom);
   }
 
   handleMove(mapMoveEvent) {
-    // console.log("hover: ", mapMoveEvent);
     this.getHucData(
       "HUC_12",
       mapMoveEvent.latlng.lat,
@@ -281,13 +281,9 @@ export class MapService {
     );
   }
 
-  getStream(lat, lng): Observable<any> {
-    const service_url = "https://ofmpub.epa.gov/waters10/";
-
-    let pPoint = "POINT(" + lng + " " + lat + ")"; // this is the point "POINT(lng lat)"
+  getStreamStart(lat, lng): Observable<any> {
     let data = {
-      // pGeometry: "POINT(" + lng + " " + lat + ")",
-      pGeometry: pPoint,
+      pGeometry: "POINT(" + lng + " " + lat + ")",
       pGeometryMod: "WKT,SRSNAME=urn:ogc:def:crs:OGC::CRS84",
       pPointIndexingMethod: "DISTANCE",
       pPointIndexingRaindropDist: 0,
@@ -297,7 +293,7 @@ export class MapService {
     };
 
     const request =
-      service_url + "PointIndexing.Service?" + this.serialize(data);
+      this.watersUrl + "PointIndexing.Service?" + this.serialize(data);
 
     return this.http.get(request).pipe(
       map((data: any) => {
@@ -312,9 +308,7 @@ export class MapService {
     );
   }
 
-  // Callback function on service completion
-  get_callback(err, response): void {
-    const service_url = "https://ofmpub.epa.gov/waters10/";
+  getStreamSegments(err, response): void {
     if (err) {
       console.log("ERROR: " + err);
     }
@@ -347,14 +341,16 @@ export class MapService {
     };
 
     this.http
-      .get(service_url + "UpstreamDownstream.Service?" + this.serialize(data))
+      .get(
+        this.watersUrl + "UpstreamDownstream.Service?" + this.serialize(data)
+      )
       .subscribe((data) => {
-        this.UD_get_callback(null, data);
+        this.buildStreamLayer(data);
       });
   }
 
-  UD_get_callback(err, response) {
-    let fl = response.output.flowlines_traversed;
+  buildStreamLayer(data) {
+    let fl = data.output.flowlines_traversed;
     let streamColor = "#00F0F0";
     let startColor = "#FF0000";
 
@@ -374,11 +370,12 @@ export class MapService {
       });
     }
 
-    // draw events_encountered
+    // build station layer
     this.stationLayer = L.geoJSON().addTo(this.map);
-    if (response.output.events_encountered) {
-      for (let i = 0; i < response.output.events_encountered.length; i++) {
-        const sEvent = response.output.events_encountered[i];
+    const stations = data.output.events_encountered;
+    if (stations) {
+      for (let i = 0; i < stations.length; i++) {
+        const sEvent = stations[i];
         const sFeatureId = sEvent.source_featureid;
         const sProgram = sEvent.source_program;
         // console.log("event: ", sEvent);
@@ -431,190 +428,3 @@ export class MapService {
     return data;
   }
 }
-
-/* these are from https://ceamdev.ceeopdev.net/static_qed/hms/js/
-  
-function get_nhd_layer_queries(bbox){
-    let base_url = nhd_plus_layers["url"];
-    let huc_layers = nhd_plus_layers["layers"];
-    let layer_queries = {};
-    for(let [key, value] of Object.entries(huc_layers)){
-        let q = "dynamicLayers=" + encodeURIComponent(JSON.stringify(value["dynamicLayers"])) +
-            "&dpi=" + value["dpi"] +
-            "&transparent=" + value["transparent"] +
-            "&format=" + value["format"] +
-            "&layers=" + value["layers"] +
-            "&bbox=" + bbox["_southWest"]["lng"] + + "," + bbox["_southWest"]["lat"] + "," + bbox["_northEast"]["lng"] + "," + bbox["_northEast"]["lat"]  +
-            "&bboxSR=900913" +
-            "&imageSR=" + value["imageSR"] +          //900913
-            "&size=" + value["size"] +
-            "&_ts=" + value["_ts"] +
-            "&f=" + value["f"];
-        layer_queries[key] = base_url + q;
-    }
-    return layer_queries;
-}
-
-function getStreamData(lat, lng) {
-    // COMID Request
-
-    var url = "https://watersgeo.epa.gov/arcgis/rest/services/NHDPlus_NP21/Catchments_NP21_Simplified/MapServer/0/query?where=&text=&objectIds=&time=&geometry=%7B%22x%22+%3A+"
-        + lng + "%2C+%22y%22+%3A+" + lat + "%2C+%22spatialReference%22+%3A+%7B%22wkid%22+%3A+4326%7D%7D&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelWithin&relationParam=&outFields=*&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=%7B%22wkt%22+%3A+%22GEOGCS%5B%5C%22GCS_WGS_1984%5C%22%2CDATUM%5B%5C%22D_WGS_1984%5C%22%2C+SPHEROID%5B%5C%22WGS_1984%5C%22%2C6378137%2C298.257223563%5D%5D%2CPRIMEM%5B%5C%22Greenwich%5C%22%2C0%5D%2C+UNIT%5B%5C%22Degree%5C%22%2C0.017453292519943295%5D%5D%22%7D&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&queryByDistance=&returnExtentsOnly=false&datumTransformation=&parameterValues=&rangeValues=&f=geojson";
-    $.ajax({
-        type: "GET",
-        url: url,
-        jsonp: true,
-        async: false,
-        success: function (data, status, jqXHR) {
-            addCatchmentToMap(data);
-        },
-        error: function (jqXHR, status) {
-            console.log("Error retrieving stream catchment data.");
-        }
-    });
-    return false;
-}
-
-function getStreamDataByComID(comid) {
-    // COMID Request
-    var catchment_base_url = "https://watersgeo.epa.gov/arcgis/rest/services/NHDPlus_NP21/Catchments_NP21_Simplified/MapServer/0/query?where=FEATUREID=" + comid;
-    var catchment_url_options = "&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=%7B%22wkt%22+%3A+%22GEOGCS%5B%5C%22GCS_WGS_1984%5C%22%2CDATUM%5B%5C%22D_WGS_1984%5C%22%2C+SPHEROID%5B%5C%22WGS_1984%5C%22%2C6378137%2C298.257223563%5D%5D%2CPRIMEM%5B%5C%22Greenwich%5C%22%2C0%5D%2C+UNIT%5B%5C%22Degree%5C%22%2C0.017453292519943295%5D%5D%22%7D&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&queryByDistance=&returnExtentsOnly=false&datumTransformation=&parameterValues=&rangeValues=&f=geojson";
-    var url = catchment_base_url + catchment_url_options;
-    $.ajax({
-        type: "GET",
-        url: url,
-        jsonp: true,
-        async: false,
-        success: function (data, status, jqXHR) {
-            addCatchmentToMap(data);
-        },
-        error: function (jqXHR, status) {
-            console.log("Error retrieving stream catchment data.");
-        }
-    });
-    return false;
-}
-
-// hucType => ["HUC_8", "HUC_12"]
-function getHucData(hucType, lat, lng) {
-    var baseUrl = "";
-    var point = "&geometry={\"x\":" + lng + ",\"y\":" + lat + ",\"spatialReference\":{\"wkid\":4326}}";
-    var outFields = "";
-    var params = "&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&returnGeometry=true&returnTrueCurves=false&geometryPrecision=&outSR=%7B%22wkid%22+%3A+4326%7D&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&queryByDistance=&returnExtentsOnly=false&datumTransformation=&parameterValues=&rangeValues=&f=geojson";
-
-    if (hucType === "HUC_12") {
-        baseUrl = "https://watersgeo.epa.gov/arcgis/rest/services/NHDPlus_NP21/WBD_NP21_Simplified/MapServer/0/query?where=&text=&time=";
-        outFields = "&outFields=OBJECTID%2C+Shape%2C+GAZ_ID%2C+AREA_ACRES%2C+AREA_SQKM%2C+STATES%2C+LOADDATE%2C+HUC_2%2C+HU_2_NAME%2C+HUC_4%2C+HU_4_NAME%2C+HUC_6%2C+HU_6_NAME%2C+HUC_8%2C+HU_8_NAME%2C+HUC_10%2C+HU_10_NAME%2C+HUC_12%2C+HU_12_NAME%2C+HU_12_TYPE%2C+HU_12_MOD%2C+NCONTRB_ACRES%2C+NCONTRB_SQKM%2C+HU_10_TYPE%2C+HU_10_MOD%2C+Shape_Length%2C+Shape_Area";
-    }
-        // else if (hucType === "HUC_10"){
-        //     baseUrl = "https://watersgeo.epa.gov/arcgis/rest/services/NHDPlus_NP21/WBD_NP21_Simplified/MapServer/1/query?where=&text=&time=";
-        //     outFields = "&outFields=OBJECTID%2C+Shape%2C+GAZ_ID%2C+AREA_ACRES%2C+AREA_SQKM%2C+STATES%2C+LOADDATE%2C+HUC_2%2C+HU_2_NAME%2C+HUC_4%2C+HU_4_NAME%2C+HUC_6%2C+HU_6_NAME%2C+HUC_8%2C+HU_8_NAME%2C+HUC_10%2C+HU_10_NAME%2C+NCONTRB_ACRES%2C+NCONTRB_SQKM%2C+HU_10_TYPE%2C+HU_10_MOD%2C+Shape_Length%2C+Shape_Area";
-    // }
-    else {
-        hucType = "HUC_8";
-        baseUrl = "https://watersgeo.epa.gov/arcgis/rest/services/NHDPlus_NP21/WBD_NP21_Simplified/MapServer/2/query?where=&text=&time=";
-        outFields = "&outFields=OBJECTID%2C+Shape%2C+GAZ_ID%2C+AREA_ACRES%2C+AREA_SQKM%2C+STATES%2C+LOADDATE%2C+HUC_2%2C+HU_2_NAME%2C+HUC_4%2C+HU_4_NAME%2C+HUC_6%2C+HU_6_NAME%2C+HUC_8%2C+HU_8_NAME%2C+Shape_Length%2C+Shape_Area";
-    }
-    var queryString = point + outFields + params;
-    getEPAWatersData(baseUrl, queryString, hucType)
-}
-
-function getHucDataById(hucID) {
-    var baseUrl = "";
-    var whereCondition = "";
-    var outFields = "";
-    var params = "&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&queryByDistance=&returnExtentsOnly=false&datumTransformation=&parameterValues=&rangeValues=&f=geojson";
-    var queryString = "";
-    if (hucID.length === 8) {
-        baseUrl = "https://watersgeo.epa.gov/arcgis/rest/services/NHDPlus_NP21/WBD_NP21_Simplified/MapServer/2/query?";
-        whereCondition = "where=HUC_8+LIKE+%28%27" + hucID + "%27%29";
-        outFields = "&outFields=OBJECTID%2C+Shape%2C+GAZ_ID%2C+AREA_ACRES%2C+AREA_SQKM%2C+STATES%2C+LOADDATE%2C+HUC_2%2C+HU_2_NAME%2C+HUC_4%2C+HU_4_NAME%2C+HUC_6%2C+HU_6_NAME%2C+HUC_8%2C+HU_8_NAME%2C+Shape_Length%2C+Shape_Area";
-        queryString = whereCondition + outFields + params;
-        getEPAWatersData(baseUrl, queryString, "HUC_8");
-    }
-    else if (hucID.length === 12) {
-        baseUrl = "https://watersgeo.epa.gov/arcgis/rest/services/NHDPlus_NP21/WBD_NP21_Simplified/MapServer/0/query?";
-        whereCondition = "where=HUC_12+LIKE+%28%27" + hucID + "%27%29";
-        outFields = "&outFields=OBJECTID%2C+Shape%2C+GAZ_ID%2C+AREA_ACRES%2C+AREA_SQKM%2C+STATES%2C+LOADDATE%2C+HUC_2%2C+HU_2_NAME%2C+HUC_4%2C+HU_4_NAME%2C+HUC_6%2C+HU_6_NAME%2C+HUC_8%2C+HU_8_NAME%2C+HUC_10%2C+HU_10_NAME%2C+HUC_12%2C+HU_12_NAME%2C+HU_12_TYPE%2C+HU_12_MOD%2C+NCONTRB_ACRES%2C+NCONTRB_SQKM%2C+HU_10_TYPE%2C+HU_10_MOD%2C+Shape_Length%2C+Shape_Area";
-        queryString = whereCondition + outFields + params;
-        getEPAWatersData(baseUrl, queryString, "HUC_12");
-    }
-}
-
-function addCatchmentToMap(data) {
-    if (currentSelectedGeometry !== null) {
-        hucMap.removeLayer(currentSelectedGeometry);
-    }
-    var hucData = data;
-    if (typeof data === "string") {
-        hucData = JSON.parse(data);
-    }
-    currentSelectedGeometry = L.geoJSON(hucData);
-    currentSelectedGeometry.addTo(hucMap);
-    hucMap.fitBounds(currentSelectedGeometry.getBounds());
-    var comid = hucData.features[0].properties.FEATUREID;
-    $('#comid').val(comid);
-    $('#selection_id').html(comid);
-    $('#selection_huc12').html(hucData.features[0].properties.WBD_HUC12);
-    $('#selection_area').html(Number(hucData.features[0].properties.AREASQKM).toFixed(4));
-    $('#selection_region').html(hucData.features[0].properties.NHDPLUS_REGION);
-    $('#add_spatial_input').removeClass("blocked");
-    setTimeout(function () {
-        if (addPopup !== null) {
-            hucMap.removeLayer(addPopup);
-        }
-        addPopup = L.popup({
-            keepInView: true,
-        }).setLatLng(hucMap.getCenter())
-            .setContent('<button id="huc_map_button_add" type="button" onclick="addSpatialInput(); toggleHucMap(); return false;">Add Catchment: ' + comid + '</button>')
-            .openOn(hucMap);
-    }, 600);
-}
-
-function addHucToMap(data, hucType) {
-    if (currentSelectedGeometry !== null) {
-        hucMap.removeLayer(currentSelectedGeometry);
-    }
-    var hucData = data;
-    if (typeof data === "string") {
-        hucData = JSON.parse(data);
-    }
-    var hucNum = hucType.slice(4, hucType.length);
-    var hucID = hucData.features[0].properties[hucType];
-    currentSelectedGeometry = L.geoJSON(hucData);
-    currentSelectedGeometry.addTo(hucMap);
-    $('#selection_id').html(hucID);
-    $('#selection_name').html(hucData.features[0].properties["HU_" + hucNum + "_NAME"]);
-    $('#selection_area').html(Number(hucData.features[0].properties.AREA_SQKM).toFixed(4));
-    $('#selection_state').html(hucData.features[0].properties.STATES);
-    hucMap.fitBounds(currentSelectedGeometry.getBounds());
-    $('#huc_id').val(hucID);
-    $('#add_spatial_input').removeClass("blocked");
-    setTimeout(function () {
-        if (addPopup !== null) {
-            hucMap.removeLayer(addPopup);
-        }
-        addPopup = L.popup({
-            keepInView: true,
-        }).setLatLng(hucMap.getCenter())
-            .setContent('<button id="huc_map_button_add" type="button" onclick="addSpatialInput(); toggleHucMap(); return false;">Add HUC: ' + hucID + '</button>')
-            .openOn(hucMap);
-    }, 600);
-}
-
-function getEPAWatersData(url, params, hucType) {
-    $.ajax({
-        type: "GET",
-        url: url + params,
-        jsonp: true,
-        async: false,
-        success: function (data, status, jqXHR) {
-            addHucToMap(data, hucType);
-        },
-        error: function (jqXHR, status) {
-            console.log("Error retrieving stream segment data.");
-        }
-    });
-}
-  
-*/
