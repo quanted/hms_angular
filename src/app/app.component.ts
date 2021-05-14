e64fafc0ec8685f082bf4af0c122e32cfc66e0f7
Jason Dunken <jasondunken@yahoo.com>
2021-05-14 12:35:49 -0400

    labels

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-  title = 'HMS';
+export class AppComponent implements OnInit {
+  title = "HMS";
+  screenWidth: number;
+  screenHeight: number;
+
+  ngOnInit(): void {
+    this.screenWidth = window.innerWidth;
+    this.screenHeight = window.innerHeight;
+    console.log(`size: ${this.screenWidth}, ${this.screenHeight}`);
+  }
+
+  @HostListener("window:resize", ["$event"])
+  onResize(event) {
+    this.screenWidth = window.innerWidth;
+    this.screenHeight = window.innerHeight;
+    console.log(`size: ${this.screenWidth}, ${this.screenHeight}`);
+  }
 }
