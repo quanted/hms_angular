7dfe03ca94d6c33acb6d24352e789566efd3a133
Jason Dunken <jasondunken@yahoo.com>
2021-05-14 14:39:21 -0400

    added preliminary run simulation request example

--- src/app/services/simulation.service.ts
+++ src/app/services/simulation.service.ts
@@ -8,6 +8,23 @@ export class SimulationService {
 
   sessionData = [];
 
+  baseJson = {};
+
+  // example API (tenative):
+  // POST ["/hms/api/workflow/"]
+  // {
+  // 	"sim_input": Main Simulation input json including all parameters (excluding boundary condition timeseries and loadings),
+  // 	"comid_inputs: {
+  // 		COMID_1: Unique input parameters for catchment=COMID_1, including boundary condition timeseries and loadings specified by the front-end user,
+  // 		COMID_2: ...
+  // 		...
+  // 	},
+  // 	"network: {
+  // 		"order": network order of execution (found from the info/stream endpoint),
+  // 		"sources: links catchment inputs dependencies to upstream catchments (parent)
+  // 	}
+  // }
+
   updateData(endpoint, data): void {
     this.sessionData[endpoint] = data;
   }
