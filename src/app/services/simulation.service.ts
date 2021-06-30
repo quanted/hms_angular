ff471ee39d0285ad587914fb528f45b6185aa97e
Jason Dunken <jasondunken@yahoo.com>
2021-06-30 16:43:42 -0400

    added scroll to loadings table

--- src/app/services/simulation.service.ts
+++ src/app/services/simulation.service.ts
@@ -23,20 +23,6 @@ export class SimulationService {
   }
 
   executeSimulation(): void {
-    // example API (tenative):
-    // POST ["/hms/api/workflow/"]
-    // {
-    // 	"sim_input": Main Simulation input json including all parameters (excluding boundary condition timeseries and loadings),
-    // 	"comid_inputs: {
-    // 		COMID_1: Unique input parameters for catchment=COMID_1, including boundary condition timeseries and loadings specified by the front-end user,
-    // 		COMID_2: ...
-    // 		...
-    // 	},
-    // 	"network: {
-    // 		"order": network order of execution (found from the info/stream endpoint),
-    // 		"sources: links catchment inputs dependencies to upstream catchments (parent)
-    // 	}
-    // }
     this.hms
       .executeAquatoxSimulation({
         sim_input: this.baseJson, // required
