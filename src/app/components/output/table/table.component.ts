e2084c8126a2a81f71aba52beb0ca7404774561e
Brandon Main <brandonlmain@gmail.com>
2021-08-09 16:13:32 -0400

    added fully responsive data table with pagination and sorting

--- src/app/components/output/table/table.component.ts
+++ src/app/components/output/table/table.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, AfterViewInit, ViewChild, Input } from "@angular/core";
-import { MatTable, MatTableDataSource } from "@angular/material/table";
+import { MatTableDataSource } from "@angular/material/table";
 import { MatPaginator } from "@angular/material/paginator";
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: "app-table",
@@ -8,9 +9,9 @@ import { MatPaginator } from "@angular/material/paginator";
   styleUrls: ["./table.component.css"],
 })
 export class TableComponent implements OnInit {
-  @ViewChild(MatTable, { static: true }) table: MatTable<any>;
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
-  /* Material table expects, it expects:
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+  /* Material table expects:
     columnNames = ["key1", "key2", ...]
     columnData = [
       { key1: value, key2: value, ... },
@@ -19,6 +20,7 @@ export class TableComponent implements OnInit {
       { key1: value, key2: value, ... }
     ] 
   */
+  @Input() catchment: string;
   @Input() columnNames: string[];
   @Input() columnData: {
     [key: string]: any;
@@ -30,12 +32,28 @@ export class TableComponent implements OnInit {
   }
 
   ngAfterViewInit(): void {
+    // Must set these after view init
+    this.dataSource.sort = this.sort;
     this.dataSource.paginator = this.paginator;
+    this.customSort();
   }
 
   ngOnChanges(): void {
+    // Must rest these again when the data changes
     this.dataSource = new MatTableDataSource();
     this.dataSource.data = this.columnData;
+    this.dataSource.sort = this.sort;
     this.dataSource.paginator = this.paginator;
+    this.customSort();
+  }
+
+  customSort() {
+    // Tell sorter Date column is a date else sort regularly
+    this.dataSource.sortingDataAccessor = (item, property) => {
+      switch (property) {
+        case 'Date': return new Date(item['Date']);
+        default: return item[property];
+      }
+    };
   }
 }
