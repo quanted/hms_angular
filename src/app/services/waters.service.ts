4b4d5ab35aabac897d7f6344d1451875879257ef
Jason Dunken <jasondunken@yahoo.com>
2021-08-30 17:04:07 -0400

    refactor waters.service

--- src/app/services/waters.service.ts
+++ src/app/services/waters.service.ts
@@ -5,124 +5,181 @@ import { Observable, of } from "rxjs";
 import { catchError, map } from "rxjs/operators";
 
 @Injectable({
-  providedIn: "root",
+    providedIn: "root",
 })
 export class WatersService {
-  watersUrl = "https://ofmpub.epa.gov/waters10/";
+    watersUrl = "https://ofmpub.epa.gov/waters10/";
+    NHDPlusUrl = "https://watersgeo.epa.gov/arcgis/rest/services/NHDPlus_NP21/";
 
-  constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient) {}
 
-  getHucData(hucType, lat, lng): Observable<any> {
-    let baseUrl = "";
-    let point =
-      '&geometry={"x":' +
-      lng +
-      ',"y":' +
-      lat +
-      ',"spatialReference":{"wkid":4326}}';
-    let outFields = "";
-    let params =
-      "&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&returnGeometry=true&returnTrueCurves=false&geometryPrecision=&outSR=%7B%22wkid%22+%3A+4326%7D&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&queryByDistance=&returnExtentsOnly=false&datumTransformation=&parameterValues=&rangeValues=&f=geojson";
-
-    if (hucType === "HUC_12") {
-      baseUrl =
-        "https://watersgeo.epa.gov/arcgis/rest/services/NHDPlus_NP21/WBD_NP21_Simplified/MapServer/0/query?where=&text=&time=";
-      outFields =
-        "&outFields=OBJECTID%2C+Shape%2C+GAZ_ID%2C+AREA_ACRES%2C+AREA_SQKM%2C+STATES%2C+LOADDATE%2C+HUC_2%2C+HU_2_NAME%2C+HUC_4%2C+HU_4_NAME%2C+HUC_6%2C+HU_6_NAME%2C+HUC_8%2C+HU_8_NAME%2C+HUC_10%2C+HU_10_NAME%2C+HUC_12%2C+HU_12_NAME%2C+HU_12_TYPE%2C+HU_12_MOD%2C+NCONTRB_ACRES%2C+NCONTRB_SQKM%2C+HU_10_TYPE%2C+HU_10_MOD%2C+Shape_Length%2C+Shape_Area";
-    }
-    // else if (hucType === "HUC_10"){
-    //     baseUrl = "https://watersgeo.epa.gov/arcgis/rest/services/NHDPlus_NP21/WBD_NP21_Simplified/MapServer/1/query?where=&text=&time=";
-    //     outFields = "&outFields=OBJECTID%2C+Shape%2C+GAZ_ID%2C+AREA_ACRES%2C+AREA_SQKM%2C+STATES%2C+LOADDATE%2C+HUC_2%2C+HU_2_NAME%2C+HUC_4%2C+HU_4_NAME%2C+HUC_6%2C+HU_6_NAME%2C+HUC_8%2C+HU_8_NAME%2C+HUC_10%2C+HU_10_NAME%2C+NCONTRB_ACRES%2C+NCONTRB_SQKM%2C+HU_10_TYPE%2C+HU_10_MOD%2C+Shape_Length%2C+Shape_Area";
-    // }
-    else {
-      hucType = "HUC_8";
-      baseUrl =
-        "https://watersgeo.epa.gov/arcgis/rest/services/NHDPlus_NP21/WBD_NP21_Simplified/MapServer/2/query?where=&text=&time=";
-      outFields =
-        "&outFields=OBJECTID%2C+Shape%2C+GAZ_ID%2C+AREA_ACRES%2C+AREA_SQKM%2C+STATES%2C+LOADDATE%2C+HUC_2%2C+HU_2_NAME%2C+HUC_4%2C+HU_4_NAME%2C+HUC_6%2C+HU_6_NAME%2C+HUC_8%2C+HU_8_NAME%2C+Shape_Length%2C+Shape_Area";
-    }
-    let queryString = point + outFields + params;
-    return this.http.get(baseUrl + queryString).pipe(
-      catchError((err) => {
-        return of({ error: err });
-      })
-    );
-  }
-
-  getCatchmentData(lat, lng): Observable<any> {
-    // COMID Request
-    let url =
-      "https://watersgeo.epa.gov/arcgis/rest/services/NHDPlus_NP21/Catchments_NP21_Simplified/MapServer/0/query?where=&text=&objectIds=&time=&geometry=%7B%22x%22+%3A+" +
-      lng +
-      "%2C+%22y%22+%3A+" +
-      lat +
-      "%2C+%22spatialReference%22+%3A+%7B%22wkid%22+%3A+4326%7D%7D&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelWithin&relationParam=&outFields=*&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=%7B%22wkt%22+%3A+%22GEOGCS%5B%5C%22GCS_WGS_1984%5C%22%2CDATUM%5B%5C%22D_WGS_1984%5C%22%2C+SPHEROID%5B%5C%22WGS_1984%5C%22%2C6378137%2C298.257223563%5D%5D%2CPRIMEM%5B%5C%22Greenwich%5C%22%2C0%5D%2C+UNIT%5B%5C%22Degree%5C%22%2C0.017453292519943295%5D%5D%22%7D&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&queryByDistance=&returnExtentsOnly=false&datumTransformation=&parameterValues=&rangeValues=&f=geojson";
-    return this.http.get(url).pipe(
-      catchError((err) => {
-        return of({ error: err });
-      })
-    );
-  }
-
-  getStreamNetworkData(comid, distance): Observable<any> {
-    let data = {
-      pNavigationType: "UT", // Upstream with tributaries
-      pStartComid: comid,
-      pTraversalSummary: "TRUE",
-      pFlowlinelist: "TRUE",
-      pEventList: "10012,10030", // 10012 - STORET, Water Monitoring | 10030 - NPGAGE, USGS Streamgages from NHDPlus
-      pEventListMod: ",",
-      pStopDistancekm: distance, // if value is null, set to default value: 50km
-      pNearestEntityList: "STORET,NPGAGE",
-      pNearestEntityListMod: ",",
-      optOutPruneNumber: 8,
-      optOutCS: "SRSNAME=urn:ogc:def:crs:OGC::CRS84",
+    params = {
+        where: "",
+        text: "",
+        time: "",
+        geometry: "",
+        geometryType: "esriGeometryPoint",
+        inSR: "",
+        spatialRel: "esriSpatialRelIntersects",
+        returnGeometry: "true",
+        returnTrueCurves: "true",
+        geometryPrecision: "",
+        outSR: "B wkid+A+4326 D",
+        returnIdsOnly: "false",
+        returnCountOnly: "false",
+        orderByFields: "",
+        groupByFieldsForStatistics: "",
+        outStatistics: "",
+        returnZ: "false",
+        gdbVersion: "",
+        returnDistinctValues: "false",
+        resultOffset: "",
+        resultRecordCount: "",
+        queryByDistance: "",
+        returnExtentsOnly: "false",
+        datumTransformation: "",
+        parameterValues: "",
+        rangeValues: "",
+        f: "geojson",
     };
 
-    return this.http
-      .get(
-        this.watersUrl + "UpstreamDownstream.Service?" + this.serialize(data)
-      )
-      .pipe(
-        catchError((err) => {
-          return of({ error: err });
-        })
-      );
-  }
+    // these are the values that the huc request will return in addition to the geometry
+    // https://watersgeo.epa.gov/arcgis/rest/services/NHDPlus_NP21/WBD_NP21_Simplified/MapServer/0
+    hucOutputFields = [
+        "OBJECTID",
+        "Shape",
+        "GAZ_ID",
+        "AREA_ACRES",
+        "AREA_SQKM",
+        "STATES",
+        "LOADDATE",
+        "HUC_2",
+        "HU_2_NAME",
+        "HUC_4",
+        "HU_4_NAME",
+        "HUC_6",
+        "HU_6_NAME",
+        "HUC_8",
+        "HU_8_NAME ",
+        "HUC_10",
+        "HU_10_NAME",
+        "HUC_12",
+        "HU_12_NAME",
+        "HU_12_TYPE",
+        "HU_12_MOD",
+        "NCONTRB_ACRES",
+        "NCONTRB_SQKM ",
+        "HU_10_TYPE",
+        "HU_10_MOD",
+        "Shape_Length",
+        "Shape_Area",
+    ];
 
-  /* from https://github.com/Esri/esri-leaflet/blob/master/src/Request.js */
-  // js object to GET request parameter string
-  serialize(params): string {
-    let data = "";
+    // these are the values that the catchment request will return in addition to the geometry
+    // https://watersgeo.epa.gov/arcgis/rest/services/NHDPlus_NP21/Catchments_NP21_Simplified/MapServer/0
+    catchmentOutputFields = [
+        "OBJECTID",
+        "Shape",
+        "Shape_Length",
+        "Shape_Area",
+        "FEATUREID",
+        "SOURCEFC",
+        "GRIDCODE",
+        "AREASQKM",
+        "WBD_HUC12",
+        "WBD_HUC12_PERCENT ",
+        "NHDPLUS_REGION",
+        "NHDPLUS_VERSION",
+    ];
 
-    params.f = params.f || "json";
+    getHucData(hucType, lat, lng): Observable<any> {
+        let url = this.NHDPlusUrl;
 
-    for (let key in params) {
-      if (params.hasOwnProperty(key)) {
-        const param = params[key];
-        const type = Object.prototype.toString.call(param);
-        let value;
-
-        if (data.length) {
-          data += "&";
+        if (hucType === "HUC_12") {
+            url += "WBD_NP21_Simplified/MapServer/0/query?";
         }
 
-        if (type === "[object Array]") {
-          value =
-            Object.prototype.toString.call(param[0]) === "[object Object]"
-              ? JSON.stringify(param)
-              : param.join(",");
-        } else if (type === "[object Object]") {
-          value = JSON.stringify(param);
-        } else if (type === "[object Date]") {
-          value = param.valueOf();
-        } else {
-          value = param;
-        }
+        const params = { ...this.params };
+        params.geometry = `{"x":${lng},"y":${lat},"spatialReference":{"wkid":4326}}`;
 
-        data += encodeURIComponent(key) + "=" + encodeURIComponent(value);
-      }
+        return this.http.get(`${url}${this.serialize(params)}&outFields=${this.hucOutputFields.join(",+")}`).pipe(
+            catchError((err) => {
+                return of({ error: err });
+            })
+        );
     }
-    return data;
-  }
+
+    getCatchmentData(lat, lng): Observable<any> {
+        let url = this.NHDPlusUrl;
+        url += "Catchments_NP21_Simplified/MapServer/0/query?";
+
+        const params = { ...this.params };
+        params.geometry = `{"x":${lng},"y":${lat},"spatialReference":{"wkid":4326}}`;
+
+        return this.http.get(`${url}${this.serialize(params)}&outFields=${this.catchmentOutputFields.join(",+")}`).pipe(
+            catchError((err) => {
+                return of({ error: err });
+            })
+        );
+    }
+
+    // this returns the stream geometry and "event" locations
+    // starting at 'comid' and ending at 'distance' upstream
+    getStreamNetworkData(comid, distance): Observable<any> {
+        let options = {
+            pNavigationType: "UT", // Upstream with tributaries
+            pStartComid: comid,
+            pTraversalSummary: "TRUE",
+            pFlowlinelist: "TRUE",
+            pEventList: "10012,10030", // 10012 - STORET, Water Monitoring | 10030 - NPGAGE, USGS Streamgages from NHDPlus
+            pEventListMod: ",",
+            pStopDistancekm: distance, // if value is null, set to default value: 50km
+            pNearestEntityList: "STORET,NPGAGE",
+            pNearestEntityListMod: ",",
+            optOutPruneNumber: 8,
+            optOutCS: "SRSNAME=urn:ogc:def:crs:OGC::CRS84",
+        };
+
+        return this.http.get(`${this.watersUrl}UpstreamDownstream.Service?${this.serialize(options)}`).pipe(
+            catchError((err) => {
+                return of({ error: err });
+            })
+        );
+    }
+
+    /* from https://github.com/Esri/esri-leaflet/blob/master/src/Request.js */
+    // js object to GET request parameter string
+    serialize(params): string {
+        let data = "";
+
+        params.f = params.f || "json";
+
+        for (let key in params) {
+            if (params.hasOwnProperty(key)) {
+                const param = params[key];
+                const type = Object.prototype.toString.call(param);
+                let value;
+
+                if (data.length) {
+                    data += "&";
+                }
+
+                if (type === "[object Array]") {
+                    value =
+                        Object.prototype.toString.call(param[0]) === "[object Object]"
+                            ? JSON.stringify(param)
+                            : param.join(",");
+                } else if (type === "[object Object]") {
+                    value = JSON.stringify(param);
+                } else if (type === "[object Date]") {
+                    value = param.valueOf();
+                } else {
+                    value = param;
+                }
+
+                data += encodeURIComponent(key) + "=" + encodeURIComponent(value);
+            }
+        }
+        return data;
+    }
 }
